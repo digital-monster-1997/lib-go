8a00334c1785f89ddd27c5ea4c98458a85b280dd
Daniel Wang <daniel_wang@trendmicro.com>
2022-09-20 14:55:30 +0800

    chore: Update annotation

--- pkg/utils/dmap/utils.go
+++ pkg/utils/dmap/utils.go
@@ -5,7 +5,8 @@ import (
 	"github.com/DigitakMonster1997/lib-go/pkg/utils/dcast"
 )
 
-// lookup Flatten a multilevel map
+// lookup Flattened multilevel map, add prefix + sep if there is one
+// If no prefix, let the depth map be concatenated using sep
 func lookup(prefix, sep string, sourceData, destData map[string]interface{}) {
 	for key, value := range sourceData {
 		fullIndex := fmt.Sprintf("%s%s%s", prefix, sep, key)
@@ -13,10 +14,10 @@ func lookup(prefix, sep string, sourceData, destData map[string]interface{}) {
 			fullIndex = fmt.Sprintf("%s", key)
 		}
 		if dd, err := dcast.ToStringMapE(value); err != nil {
-			// 最後一層了,無法分割
+			//  The last level, can not be split
 			destData[fullIndex] = value
 		} else {
-			// 有下一層，再繼續下去
+			// Have next level, go deep
 			lookup(fullIndex, sep, dd, destData)
 		}
 	}
